7a9e653516d7851b8fb2dea8aec04a5c7b631338
juan <bravo43juancho@gmail.com>
2026-04-02 13:41:48 -0300

    feat: added guard to protect action routes #21

--- src/seasons/repository/SqliteGetSeasons.ts
+++ src/seasons/repository/SqliteGetSeasons.ts
@@ -23,7 +23,7 @@ export class SqliteGetSeasons extends SqliteBaseClass implements GetSeasonsDAO {
             result.forEach((row) => {
                 const season = Season.create(
                     UUID.restore(row.id),
-                    row.isActive,
+                    row.isActive === 1,
                     new Date(row.startDate),
                     new Date(row.endDate),
                     StringObject.create(row.name),
